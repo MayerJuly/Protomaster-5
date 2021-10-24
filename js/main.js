f76f716cc0330a384a4f2eb911c10b64b79691ee
MayerJuly <73027797+MayerJuly@users.noreply.github.com>
2021-10-24 22:04:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function() {
+    $('.preloader__content').addClass('active');
+    setTimeout(function (){
+        $('.preloaderText').addClass('active');
+    },200)
+
+    let burger = $('.header__burger')
+    let nav = $('.header__nav')
+
+    burger.on('click', (e) => {
+        e.preventDefault()
+
+        nav.toggleClass('active')
+        burger.toggleClass('active')
+    })
+
+//    PRELOADER AUTO-TEXT
+    let preloaderTextInfo = $('#preloaderTextInfo')
+
+    function animateText(text) {
+        const string = text.text()
+        let n = 80;
+        if(text === preloaderTextInfo) n=40;
+        return text.each(function () {
+            const $text = $(this)
+            $text.html(string.replace(/./g, '<span class="new">$&</span>'))
+            $text.find('span.new').each(function (i, el) {
+                setTimeout(function () {
+                    $(el).addClass('div_opacity');
+                }, n * i)
+            })
+        })
+    }
+
+    function showPreloaderTextInfo(){
+        preloaderTextInfo.css('opacity', '1')
+        animateText(preloaderTextInfo)
+    }
+
+    setTimeout(function (){
+        showPreloaderTextInfo();
+    },1200)
+
+
+    $("[data-scroll]").on('click', function (event) {
+        event.preventDefault()
+
+        let elementID = $(this).data('scroll')
+        let elementOffset = $(elementID).offset().top
+
+        if (burger.hasClass('active')) {
+            nav.removeClass('active')
+            burger.removeClass('active')
+        }
+
+        $('html, body').animate({
+            scrollTop: elementOffset,
+        }, 700)
+    })
+});
