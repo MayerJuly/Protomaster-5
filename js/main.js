89965b78acc8120a8a0297c27b35223322802d46
MayerJuly <73027797+MayerJuly@users.noreply.github.com>
2021-10-25 03:11:26 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -42,19 +42,112 @@ $(function() {
     },1000)
 
 
-    $("[data-scroll]").on('click', function (event) {
-        event.preventDefault()
+    //    Modal
 
-        let elementID = $(this).data('scroll')
-        let elementOffset = $(elementID).offset().top
+    const close = $('.js-close-popup');
+    const modalOverlay = $('.overlay');
+    const popup = $('.js-popup');
+    const form = document.getElementById('form');
+    const username = document.getElementById('username');
+    const phone = document.getElementById('phone');
 
-        if (burger.hasClass('active')) {
-            nav.removeClass('active')
-            burger.removeClass('active')
+
+    function closeModal(close,object) {
+        close.click(function (){
+            object.removeClass('active')
+        })
+    }
+
+
+    function closeModalOverlay(popup,object){
+        $(document).mousedown(function (e) {
+            if (e.target !== popup[0] && popup.has(e.target).length === 0) {
+                object.removeClass('active')
+            }
+        })
+    }
+
+$('.modal-btn').click(function (){
+    modalOverlay.addClass('active')
+})
+
+
+
+    closeModal(close,modalOverlay)
+    closeModalOverlay(popup,modalOverlay)
+
+    form.addEventListener('submit',(e)=>{
+
+        e.preventDefault()
+
+        checkInputs()
+    })
+
+    function checkInputs(){
+        let msg = $('#form').serialize();
+        //    get values from inputs
+        const usernameValue = username.value.trim();
+        const phoneValue = phone.value.trim();
+
+        let error = false;
+
+        if(usernameValue === ''){
+            setErrorFor(username, 'Введите ваше имя')
+            error = true;
+
+        } else{
+            setSuccessFor(username)
         }
 
-        $('html, body').animate({
-            scrollTop: elementOffset,
-        }, 700)
-    })
+
+        if(phoneValue === ''){
+            setErrorFor(phone, 'Введите ваш номер телефона');
+            error = true;
+
+        } else if(!validatePhone(phoneValue)){
+            setErrorFor(phone, 'Неккоректный ввод номера телефона');
+            error = true;
+        }
+        else{
+            setSuccessFor(phone);
+        }
+
+        if(!error){
+            $.ajax({
+                type: "POST",
+                url: "../mail.php", //Change
+                data: msg
+            }).done(function() {
+                alert("Сообщение отправлено");
+                setTimeout(function() {
+                    form.reset()
+                    document.location.replace("index.php");
+                    form.trigger("reset");
+                }, 1000);
+            });
+            return false;
+
+        }
+    }
+
+    function setErrorFor(input,message){
+        const formControl = input.parentElement // .form__control
+        const small = formControl.querySelector('small')
+
+        // add error message
+        small.innerText = message
+        // add error class
+        formControl.classList.add('error')
+    }
+
+    function setSuccessFor(input){
+        const formControl = input.parentElement // .form__control
+        formControl.classList.remove('error')
+    }
+
+    function validatePhone(phone){
+        let regex = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+        return regex.test(phone);
+    }
+
 });
